27ace720a68bb962dad5917d62dff12f159e02d1
Steven Schlansker <sschlansker@opentable.com>
2014-07-23 16:02:37 -0700

    Commentry

--- src/pause.js
+++ src/pause.js
@@ -4,6 +4,8 @@ var callbacks = [];
 
 PauseDetector = {}
 
+// Tick repeatedly.  If a VM pause happens, ticks
+// will come dramatically late, and then we sound the alarm!
 function runCheck () {
   var now = Date.now();
   var timeSinceLastCheck = now - lastPause;
@@ -18,25 +20,30 @@ function runCheck () {
 
 var timer;
 
+// Start detection
 PauseDetector.start = function() {
   timer = setInterval(runCheck, 25);
   timer.unref();
   logger.info("Detecting Node VM pauses");
 }
 
+// Stop detection
 PauseDetector.stop = function() {
   logger.info("Stop detecting Node VM pauses");
   clearInterval(timer);
 }
 
+// Register additional callbacks
 PauseDetector.onPause = function(callback) {
   callbacks.push(callback);
 }
 
 module.exports = PauseDetector;
 
+// By default, log an error on pause
 PauseDetector.onPause(function(ms) {
   logger.error("The Node runtime paused for %s ms! (+/- 25)", ms);
 });
 
+// Start running on module include!
 PauseDetector.start();
