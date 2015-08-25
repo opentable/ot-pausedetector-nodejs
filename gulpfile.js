0539efd45a9cd049ddb2afc8ea4443891985fa18
Tor Helmer <torhelmer@gmail.com>
2015-08-24 19:41:36 -0700

    settings for ot-pausedetector-nodejs

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,15 @@
 // Include gulp
 var gulp = require('gulp');
 
-var scripts = ['lib/*.js'];
+var scripts = ['src/*.js'];
 var tests = ['test/*.js'];
 var coverage = 'coverage/';
 var mochaOpts = {};
 var coverageThresholds = {
-    statements: 77,
+    statements: 84,
     branches: 60,
-    functions: 83,
-    lines: 80
+    functions: 87,
+    lines: 86
   };
 
 require('../ot-gulp-release-tasks/index')(gulp, scripts, tests, mochaOpts, coverageThresholds);
