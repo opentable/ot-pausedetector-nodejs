9478b591c56d70df33355cb2739f814726127e6c
Tor Helmer <torhelmer@gmail.com>
2015-08-24 19:41:58 -0700

    lint fixes

--- test/pause-test.js
+++ test/pause-test.js
@@ -1,10 +1,11 @@
-assert = require('assert');
+'use strict';
+var assert = require('assert');
 
 var log = function(msg) {
   console.log(msg);
 };
 
-logger = {
+var logger = {
   debug: log,
   info: log,
   warn: log,
@@ -28,26 +29,29 @@ describe('PauseDetector', function() {
     var now = Date.now();
 
     logger.info("Start 150ms pause");
+    /* jshint ignore:start */
     while (Date.now() - now < 150) { }
     logger.info("End 150ms pause");
-
+    /* jshint ignore:end */
     setTimeout(function() {
       assert.equal(pauses.length, 1);
       assert.ok(pauses[0] > 150 && pauses[0] < 200);
 
       now = Date.now();
       logger.info("Start 15ms pause");
+      /* jshint ignore:start */
       while (Date.now() - now < 15) { }
       logger.info("End 15ms pause");
-
+      /* jshint ignore:end */
       setTimeout(function() {
         assert.equal(pauses.length, 1);
 
         var now = Date.now();
         logger.info("Start 650ms pause");
+        /* jshint ignore:start */
         while (Date.now() - now < 650) { }
         logger.info("End 650ms pause");
-
+        /* jshint ignore:end */
         setTimeout(function() {
           assert.equal(pauses.length, 2);
           assert.ok(pauses[1] > 650 && pauses[1] < 700);
